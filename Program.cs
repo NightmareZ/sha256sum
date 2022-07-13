2393abf11fce140263af207716e07d885f18549d
Mykhailo Makarov <m.m.makarov@gmail.com>
2022-07-13 15:25:07 +0200

    Minimal realization.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+static string ByteArrayToString(byte[] array)
+{
+    var sb = new StringBuilder();
+
+    foreach (var b in array)
+    {
+        sb.Append(string.Format($"{b:X2}"));
+    }
+
+    return sb.ToString().Trim().ToLowerInvariant();
+}
+
+static string CalculateHashForFile(string fileName)
+{
+    using SHA256 sha256 = SHA256.Create();
+    using FileStream fileStream = File.OpenRead(fileName);
+    fileStream.Position = 0;
+    byte[] hashValue = sha256.ComputeHash(fileStream);
+    return ByteArrayToString(hashValue);
+}
+
+string fileName = args[0];
+string hash = CalculateHashForFile(fileName);
+Console.WriteLine(hash);
